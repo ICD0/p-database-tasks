ee0cb65827f7f0451a189c7759d662d8b9522a5e
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-11-23 02:31:16 -0600

    :sparkles: Add new user fields

--- init.sql
+++ init.sql
@@ -30,6 +30,8 @@ CREATE TABLE auth.users
     user_id  uuid        NOT NULL,
     username varchar(50) NOT NULL UNIQUE,
     email    varchar(50) NOT NULL UNIQUE,
+    creation_at timestamp,
+    updated_at timestamp,
     CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
 
